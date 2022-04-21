0fe967dc788eb586d529618830ad4afa75d89c3e
Masud8851 <masud1971.cse@gmail.com>
2022-04-22 02:32:55 +0600

    Slick

--- main.js
+++ main.js
@@ -10,8 +10,49 @@ $(document).ready(function () {
 });
 
 // multiple items slides
-$(".multiple-items").slick({
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
+if ($(window).width() <= 320) {
+	$(".sectionTwo_slider").slick({});
+} else {
+	$(".sectionTwo_slider").slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 3,
+	});
+}
+
+// Responsive Display
+$(".sectionThree_slider").slick({
+	dots: true,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true,
+			},
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			},
+		},
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	],
 });
